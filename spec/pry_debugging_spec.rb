9160ddabaf06c7bd4ca3af8b196e0b1c63a720ce
matthewjstahnke <matthewjstahnke@gmail.com>
2020-11-10 18:00:16 -0600

    commit tree ac8def23dda7d0346bf5640aad00e484cc1d2d7f

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -2,6 +2,8 @@ require 'pry'
 require './lib/pry_debugging.rb'
 
 describe "#plus_two" do 
+binding.pry
+
 	it "takes in a number as an argument and returns the sum of that number and 2" do
 		expect(plus_two(3)).to eq(5)
 	end
